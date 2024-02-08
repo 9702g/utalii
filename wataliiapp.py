cc5a9a1078ae6ac1f98afc9530eccca80973bcf6
9702g <91326552+9702g@users.noreply.github.com>
2024-02-08 21:20:55 +0300

    Update wataliiapp.py

--- wataliiapp.py
+++ wataliiapp.py
@@ -83,8 +83,8 @@ print('Mean absolute Error:', score_2)
 
 import pickle
 
- Save the trained model to a .pkl file using pickle
-model_file_path = "V:\\my project\\xgb_model.pkl"
+Save the trained model to a .pkl file using pickle
+model_file_path = ("V:\\my project\\xgb_model.pkl")
 with open(model_file_path, 'wb') as file:
     pickle.dump(model, file)
 
