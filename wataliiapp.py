210adb259fdb60cc08c54165078a2163f98ee635
9702g <91326552+9702g@users.noreply.github.com>
2024-02-08 20:33:35 +0300

    Update wataliiapp.py

--- wataliiapp.py
+++ wataliiapp.py
@@ -6,7 +6,7 @@ import pickle
 # Header
 st.header("Kenya Tourism Expenditure Prediction")
 st.subheader("A simple machine learning app to predict how much money a tourist will spend when visiting Kenya.")
-st.image("V:\\Kenyan data\\new_tou_app\\images (1).jpg")
+st.image("V:\\Git_repo\\utalii\\images (1).jpg")
 
 # Form
 my_form = st.form(key="financial_form")
